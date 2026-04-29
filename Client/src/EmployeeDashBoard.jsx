ef8ff800e262eb51f680183395389ce966ba8c40
Rohith Vittamraj <158805322+rohith-2809@users.noreply.github.com>
2026-04-29 12:17:56 +0530

    feat(fix) : OOM error fixed

--- Client/src/EmployeeDashBoard.jsx
+++ Client/src/EmployeeDashBoard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import clsx from "clsx"; 
+import clsx from "clsx";
 import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useState, useRef, memo } from "react";
 import {
